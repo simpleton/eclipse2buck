471ebdb6dec0344461e62dbb59256aef669f2d47
simpleton <sunsj1231@gmail.com>
2014-03-21 02:41:30 +0800

    refactor jar class

---
+++ gen_jars.py
@@ -0,0 +1,25 @@
+from gen_base_target import BaseTarget
+import decorator
+import util
+import os
+class Jars(BaseTarget):
+    """
+    generated all jar's targets
+    """
+    jar_list = []
+    def __init__(self, root, name):
+        BaseTarget.__init__(self, root, name, "_JAR")
+        folder = self.lib_path
+        self.jar_list = util.find_all_files_with_suffix(folder, "*.jar")
+        
+    @decorator.target("prebuilt_jar")
+    def gen_jar(self, name, relative_path):
+        print "name = '%s'," % name
+        print "binary_jar = '%s'," % relative_path
+        print "visibility = [ 'PUBLIC' ],"
+
+    def dump(self):
+        for jar_path in self.jar_list:
+            name = util.path_get_basename(jar_path)
+            self.gen_jar(name, jar_path)
+            self.exported_deps.append(":%s" % name)
---
+++ test_jars.py
@@ -0,0 +1,6 @@
+from gen_jars import Jars
+
+if __name__ == "__main__":
+    jar = Jars('../', 'libnetscene')
+    jar.dump()
+    print jar.deps, jar.exported_deps
