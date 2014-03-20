95e36edda091a154a8f2fb59526353bb5d6282de
simpleton <sunsj1231@gmail.com>
2014-03-21 03:00:33 +0800

    refactor native class

---
+++ gen_native.py
@@ -0,0 +1,27 @@
+from gen_base_target import BaseTarget
+import decorator
+import util
+import os
+
+class NativeLib(BaseTarget):
+    """
+    gen native lib target
+    """
+    def __init__(self, root, name):
+        BaseTarget.__init__(self, root, name, "_NATIVE")
+        
+    def dump(self):
+        if self._is_exist_native():
+            name = self.target_name(self.proj_name)
+            self.gen_native_lib(name)
+            self.deps.append(name)
+    
+
+    @decorator.target("prebuilt_native_library")
+    def gen_native_lib(self, name):
+        print "name = '%s'," % name
+        print "native_libs = 'libs',"
+
+    def _is_exist_native(self):
+        native_lib_path = os.path.join(self.lib_path, "libs")
+        return os.path.isdir(native_lib_path) and len(util.find_all_files_with_suffix(native_lib_path, "*.so")) > 0
---
+++ test_native.py
@@ -0,0 +1,6 @@
+from gen_native import NativeLib
+
+if __name__ == "__main__":
+    nativelib = NativeLib("../", "libnetscene")
+    nativelib.dump()
+    print nativelib.deps, nativelib.exported_deps
