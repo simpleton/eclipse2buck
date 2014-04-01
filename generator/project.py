b726cfdcb1ef98967435058aecb04d42110fe8ed
simpleton <sunsj1231@gmail.com>
2014-04-01 22:48:04 +0800

    check bin is contained in list before remove it

--- generator/project.py
+++ generator/project.py
@@ -97,7 +97,8 @@ class LibProject(BaseTarget):
         ## get all folders that contained JAVA file
         folders = util.find_all_folder_contains_file_with_suffix(os.path.join(self.root, self.proj_name), '*.java')
         ##ignore bin folder
-        folders.remove('bin')
+        if 'bin' in folders:
+            folders.remove('bin')
         ##print srcs target
         print "srcs = glob([%s])" % self.print_all_java(folders)
 
